cf3b1184e54e98f5a643a5c0cfe4e8d755c77f2e
Britt Gresham <brittcgresham@gmail.com>
2014-07-01 15:09:50 +0000

    Added !define and setup.py

--- modules/definer.py
+++ modules/definer.py
@@ -6,13 +6,60 @@ import re
 import itertools
 
 import sqlalchemy as sa
+import nltk
+from nltk.corpus import wordnet as wn
 
-from modules.quote import DBSession, Message
+from modules.quote import DBSession, Message, User, Room
+
+tokenizer = nltk.tokenize.RegexpTokenizer("[\w'-]+")
+#tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
+
+
+def get_synonyms(word):
+    similar_words = [x.similar_tos() for x in wn.synsets(word)]
+    similar_words = [item.lemma_names for l in similar_words for item in l]
+    suggestions = [word for l in similar_words for word in l]
+    return suggestions
 
 
 def action(msg):
     msg = '\x01ACTION %s\x01' % msg
     return msg
+
+
+def word_definer(phenny, input):
+    target = input.groups()[0]
+    definitions = enumerate(wn.synsets(target), start=1)
+    definitions = [
+        "%d (%s): %s" % (index, synset.lemma_names[0], synset.definition) for index, synset in definitions
+    ]
+    phenny.say(". ".join(definitions))
+word_definer.rule = r"^!define (\w+)"
+word_definer.priority = "medium"
+
+
+def synonymize(phenny, input):
+    target = input.groups()[1].lower()
+    words = tokenizer.tokenize(target)
+    if len(words) == 1:
+        nick = words[0]
+        message = DBSession.query(Message).join(User).join(Room).filter(
+            User.nick.ilike("%s%%" % target)
+        ).filter(
+            Room.name == input.sender
+        ).filter(
+            sa.not_(Message.body.like("!%"))
+        ).order_by(Message.created_at.desc()).first()
+        if message:
+            target = message.body
+            words = tokenizer.tokenize(target)
+    for word in words:
+        suggestions = get_synonyms(word)
+        if suggestions:
+            target = target.replace(word, random.choice(suggestions), 1)
+    phenny.say(target)
+synonymize.rule = r"^!(synonymize|syn|rejigro) (.+)"
+synonymize.priority = "medium"
 
 
 def define(phenny, input):
@@ -124,5 +171,5 @@ def define(phenny, input):
             search,
         )
     phenny.say(" ".join([x.replace(" ", "") for x in msg.split(" ") if not(x.replace(" ", "") == "")]))
-define.rule = r"$nickname\W? (what|when|where|why|which|who|how|can|did|does) ([a-zA-Z']+) ([a-zA-Z0-9 \-,'\"]+)\??"
+define.rule = r"$nickname\W? (what|when|where|why|which|who|how|can|did|does|are|is) ([a-zA-Z']+) ([a-zA-Z0-9 \-,'\"]+)\??"
 define.priority = "medium"
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+setup(
+    name = "#r/kansascity Bot",
+    version = "1.0.0",
+    author = "Britt Gresham and Contributors",
+    author_email = "brittcgresham@gmail.com",
+    description = ("A bot for doing things in the IRC"),
+    license = "MIT",
+    install_requires=[
+        "sqlalchemy",
+        "nltk",
+        "numpy",
+        "pyyaml",
+    ],
+)
